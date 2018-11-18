df55a7fed8caff67c6f463f5bd632a8155f6f384
Adrian Blanco <adrian.blanco@mobgen.com>
2018-11-18 23:02:56 +0100

    Modified the way to show the classification

--- commands-business/classification-business.js
+++ commands-business/classification-business.js
@@ -1,27 +1,12 @@
 const { getClassification } = require('../commands-repository/classification-repository');
 
-function getPosition(position, last) {
-  switch(position) {
-    case 0:
-      return ':first_place_medal:';
-    case 1:
-      return ':second_place_medal:';
-    case 2:
-      return ':third_place_medal:';
-    case last - 1:
-      return ':poop:';
-    default:
-      return `  ${position + 1}.\t`;
-  }
-}
-
 function generateSpace(name) {
   const SPACE = 30;
-  const spaceNeeded = (SPACE - name.length)/ 3;
+  const spaceNeeded = (SPACE - name.length);
   let i = 0;
   let spaces = '';
   while(i < spaceNeeded) {
-    spaces += '\t';
+    spaces += ' ';
     i++;
   }
   return spaces;
@@ -30,10 +15,11 @@ function generateSpace(name) {
 async function addResult(channel) {
   try {
     const classification = await getClassification(channel);
-    let message = `*=== Classification of ${channel} ===*\n`;
+    let message = `\`\`\`======= Classification of ${channel} =======\n`;
     classification.forEach((player, position) => {
-      message += `${getPosition(position, classification.length)} ${player.name} ${generateSpace(player.name)} \t ${player.points} \n`;
+      message += `${position + 1}. ${player.name} ${generateSpace(player.name)} ${player.points} \n`;
     });
+    message += '\`\`\`';
     return message;
   } catch (err) {
     return 'We are having some problems retrieving stored data... (' + err + ')'
